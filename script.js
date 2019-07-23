a2bf405cdf368535101d83578f042070d387da6a
Sam <sam@simplprimate.me>
2019-07-23 17:56:38 -0600

    background_generator

--- script.js
+++ script.js
@@ -1,39 +1,19 @@
-var button = document.getElementById("enter");
-var input = document.getElementById("userinput");
-var ul = document.querySelector("ul");
-var li = document.querySelectorAll("li");
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
 
-function strikeThrough(e) {
-  if (e.target.tagName === "LI") {
-      e.target.classList.toggle("done");
-  }
+function setGradient() {
+  body.style.background = 
+    "linear-gradient(to right, " 
+    + color1.value 
+    + ", " 
+    + color2.value 
+    + ")";
+
+    css.textContent = body.style.background + ";";
 }
 
-function inputLength() {
-  return input.value.length;
-}
+color1.addEventListener("input", setGradient);
 
-function createListElement() {
-  var li = document.createElement("li");
-    li.appendChild(document.createTextNode(input.value));
-    ul.appendChild(li);
-    input.value = "";
-}
-
-function addListAfterClick() {
-  if (inputLength() > 0) {
-    createListElement();
-  } 
-}
-
-function addListAfterKeyPress(event) {
-  if (inputLength() > 0 && event.keyCode === 13) {
-    createListElement();
-  }
-}
-
-button.addEventListener("click", addListAfterClick);
-
-input.addEventListener("keypress", addListAfterKeyPress);
-
-ul.addEventListener("click", strikeThrough);
+color2.addEventListener("input", setGradient);
