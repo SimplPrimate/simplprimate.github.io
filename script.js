8360f06cbe8dbc9ffb337a594c874ee3402e6afb
Sam <sam@simplprimate.me>
2019-07-23 20:40:32 -0600

    Update script.js

--- script.js
+++ script.js
@@ -3,38 +3,34 @@ var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
 var button = document.getElementById("button");
-const randomNum = () => Math.floor(Math.random() *  255);
+
+function setColorsAndText() {
+	var css = document.querySelector('h3');
+
+	body.style.background = 'linear-gradient(to right, ' + color1.value + ', ' + color2.value + ')';
+	css.textContent = body.style.background + ';';
+}
+
+function getRandomColor() {
+	var letters = '0123456789ABCDEF'.split('');
+	var color = '#';
+
+	for (var i = 0; i < 6; i++) {
+		color += letters[Math.floor(Math.random() * 16)];
+	}
+
+	return color;
+}
 
 function randomizeColor() {
-  var rgb = 'rgb(' + randomNum() + ',' + randomNum() + ',' + randomNum() + ')';
-  var rgb2 = 'rgb(' + randomNum() + ',' + randomNum() + ',' + randomNum() + ')';
-  body.style.background = 
-    "linear-gradient(to right, " 
-    + rgb 
-    + ", " 
-    + rgb2
-    + ")";
-    // color1.value = rgb;
-    // color2.value = rbg2;
+    color1.value = getRandomColor();
+    color2.value = getRandomColor();
+    
+    setColorsAndText();
+  }
 
-    css.textContent = body.style.background + ";";
-}
 
-function setGradient() {
-  body.style.background = 
-    "linear-gradient(to right, " 
-    + color1.value 
-    + ", " 
-    + color2.value 
-    + ")";
-
-    css.textContent = body.style.background + ";";
-}
-
-setGradient();
-
-color1.addEventListener("input", setGradient);
-
-color2.addEventListener("input", setGradient);
-
+body.addEventListener("onload", setColorsAndText());
+color1.addEventListener('input', setColorsAndText);
+color2.addEventListener('input', setColorsAndText);
 button.addEventListener("click", randomizeColor);
