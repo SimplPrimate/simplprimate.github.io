70a56d269441f282be491a433bf054362ca73085
Sam <sam@simplprimate.me>
2019-07-23 19:15:35 -0600

    Updating Files

--- script.js
+++ script.js
@@ -2,7 +2,19 @@ var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
+var button = document.getElementById("button");
+const randomNum = () => Math.floor(Math.random() *  255);
 
+function randomizeColor() {
+  body.style.background = 
+    "linear-gradient(to right, " 
+    + randomNum(); 
+    + ", " 
+    + randomNum(); 
+    + ")";
+
+    css.textContent = body.style.background + ";";
+}
 
 function setGradient() {
   body.style.background = 
@@ -20,3 +32,5 @@ setGradient();
 color1.addEventListener("input", setGradient);
 
 color2.addEventListener("input", setGradient);
+
+button.addEventListener("click", randomizeColor);
